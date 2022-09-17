bcd5d54f55e1217c85691bdaf82ae48feaae5739
pockerman <a.giavaras@gmail.com>
2022-09-17 08:28:33 +0100

    Update config file

---
+++ mir_app/config.py
@@ -0,0 +1,6 @@
+from pathlib import Path
+
+MIR_APP_REST_API = 'http://127.0.0.1:8000/api/v1/'
+SURVEYS_PATH = Path("/home/alex/qi3/mir_app_web_app/surveys")
+TOTAL_IMAGE_COUNTER = 2
+MIN_NUM_IMG = 1
